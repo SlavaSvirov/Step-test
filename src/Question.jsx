0229c0ddeac2aa89d4d23d497f227d52bd87fe6c
SlavaSvirov <slavasvirov@yandex.ru>
2021-02-14 23:24:05 +0300

    main logic

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-export const Question = ({ item }) => {
+export const Question = ({ item, onChangeIsFilled, onUserAnswer }) => {
+  // const isValid = item.answer.toLowerCase() === userAnswer.toLowerCase();
+
+  const handleInputChange = e => {
+    onUserAnswer(e.target.value);
+
+    onChangeIsFilled({ [item.id]: !!e.target.value });
+  };
+
   return (
     <div>
       {item.question}
       <div>
-        <input type="text" />
+        <input onChange={handleInputChange} type="text" />
       </div>
     </div>
   );
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Question } from "./Question";
+import "./Test.css";
 
 const data = [
   { id: 1, question: "2+2?", answer: 4 },
@@ -7,14 +8,28 @@ const data = [
   { id: 3, question: "20-10?", answer: 10 }
 ];
 
+const initialAnswers = data.reduce((acc, el) => {
+  return { ...acc, [el.id]: false };
+}, {});
+
 export const Test = () => {
   const [step, setStep] = React.useState(0);
   const [items, setItems] = React.useState(data);
+  const [answers, setAnswers] = React.useState(initialAnswers);
   const [isPassed, setIsPassed] = React.useState(false);
-
+  const [userAnswer, setUserAnswer] = React.useState("");
+  console.log(userAnswer);
   const handleIncrease = () => setStep(prevStep => prevStep + 1);
   const handleDecrease = () => setStep(prevStep => prevStep - 1);
   const handlePassTest = () => setIsPassed(true);
+
+  const isDisabled = Object.keys(answers).some(a => {
+    return !answers[a];
+  });
+
+  const handleChangeIsFilled = questionObj => {
+    setAnswers({ ...answers, ...questionObj });
+  };
 
   React.useEffect(() => {
     setItems(data);
@@ -24,20 +39,41 @@ export const Test = () => {
     <div>
       {!isPassed ? (
         <>
-          <Question item={{ ...data[step] }} />
-          <button onClick={handleDecrease}>Назад</button>
-          <button onClick={handleIncrease}>Вперед</button>
+          <Question
+            onUserAnswer={setUserAnswer}
+            onChangeIsFilled={handleChangeIsFilled}
+            item={{ ...data[step] }}
+          />
+          {step !== 0 ? (
+            <button onClick={handleDecrease}>Назад</button>
+          ) : (
+            <div></div>
+          )}
+          {step !== items.length - 1 ? (
+            <button onClick={handleIncrease}>Вперед</button>
+          ) : (
+            <div></div>
+          )}
         </>
       ) : (
         <>
           <div>
             {items.map(i => (
-              <div> {i.question}</div>
+              <div>
+                <div> {i.question} </div>
+                <div>{userAnswer}</div>
+              </div>
             ))}
           </div>
         </>
       )}
-      <button onClick={handlePassTest}>Сдать тест</button>
+      {!isDisabled ? (
+        <button onClick={handlePassTest}>Проверить ответы</button>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
+
+// После того, как пользователь ответит на все вопросы, должна появится кнопочка 'проверить ответы'. По нажатию на эту кнопочку нужно вывести все вопросы на экран, под вопросами должно быть следующее: 'ваш ответ такой-то, правильно' или 'ваш ответ такой-то, не правильно, правильный ответ такой-то'. Правильные ответы должны быть зеленого цвета, а неправильные - красного.
